b819901577b99ea13abb806d7aaca9bb09613216
Kyle <prophet@hanyang.ac.kr>
2023-08-02 02:18:22 +0900

    test(MyBatis): JUnit Test Case for MyBatis & DBCP - DataSourceTest.java

--- src/main/java/kr/co/dbcs/config/RootConfig.java
+++ src/main/java/kr/co/dbcs/config/RootConfig.java
@@ -1,8 +1,8 @@
 package kr.co.dbcs.config;
 
-import com.zaxxer.hikari.HikariDataSource;
 import lombok.AllArgsConstructor;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,21 +30,24 @@ public class RootConfig {
     public DataSource dataSource() {
         // DBCP 설정
 
-        HikariDataSource dataSource = new HikariDataSource();
+        BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName(env.getProperty("oracle.jdbc.driverClassName"));
-        dataSource.setJdbcUrl(env.getProperty("oracle.jdbc.url"));
+        dataSource.setUrl(env.getProperty("oracle.jdbc.url"));
         dataSource.setUsername(env.getProperty("oracle.jdbc.username"));
         dataSource.setPassword(env.getProperty("oracle.jdbc.password"));
 
-        // HikariCP 상세 속성 설정
-        dataSource.setMinimumIdle(10); // 최소 유휴 연결 수
-        dataSource.setMaximumPoolSize(20); // 최대 풀 크기
-        dataSource.setIdleTimeout(30000); // 유휴 연결 유지 시간(밀리 초); 30000 밀리 초 (=30초)
-        dataSource.setConnectionTimeout(30000); // 연결 시간 제한(밀리 초); 30000 밀리 초 (=30초)
-
-        // HikariCP의 keepAlive 설정
-        dataSource.setConnectionTestQuery("SELECT 1 FROM DUAL");
-        dataSource.setKeepaliveTime(60000); // KeepAlive 쿼리 실행 주기(밀리 초); 60000 밀리 초 (60초) 예시
+        // DBCP 상세 속성 설정
+        dataSource.setMinIdle(10); // 최소 유휴 연결 수
+        dataSource.setMaxTotal(20); // 최대 풀 크기
+        dataSource.setMaxIdle(30); // 유휴 연결 유지 시간(밀리 초); 30000 밀리 초 (=30초)
+        dataSource.setValidationQuery("SELECT 1 FROM DUAL");
+        dataSource.setValidationQueryTimeout(10); // 검증 쿼리 타임아웃 (초)
+        dataSource.setTestOnBorrow(true); // 대여 전에 검증 쿼리 수행
+        dataSource.setTestOnReturn(true); // 반환 후에 검증 쿼리 수행
+        dataSource.setTestWhileIdle(true); // 유휴 상태일 때 검증 쿼리 수행
+        dataSource.setTimeBetweenEvictionRunsMillis(60000); // 검사 간격 (밀리 초); 60000 밀리 초 (=1분)
+        dataSource.setNumTestsPerEvictionRun(3); // 각 검사마다 실행되는 검증 쿼리 수
+        dataSource.setMinEvictableIdleTimeMillis(1800000); // 제거 될 수 있는 최소 유휴 시간 확인 (밀리 초); 1800000 밀리 초 (=30분)
 
         return dataSource;
     }
---
+++ src/test/java/kr/co/dbcs/config/DataSourceTest.java
@@ -0,0 +1,57 @@
+package kr.co.dbcs.config;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = RootConfig.class)
+class DataSourceTest {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    SqlSessionFactory sqlSessionFactory;
+
+    @Test
+    void dataSourceNotNullTest() {
+        assertNotNull(dataSource, "dataSource == null");
+    }
+
+    @Test
+    void dbConnectionTest() {
+        try (Connection connection = dataSource.getConnection()) {
+            assertNotNull(connection, "connection == null");
+        } catch (SQLException e) {
+            throw new RuntimeException("DB Connection 실패", e);
+        }
+    }
+
+    @Test
+    void testSelect1FromDualUsingMyBatis() {
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            Connection connection = session.getConnection();
+            try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT 1 FROM DUAL");
+                 ResultSet resultSet = preparedStatement.executeQuery()) {
+                assertTrue(resultSet.next(), "resultSet 결과 없음");
+                int result = resultSet.getInt(1);
+                assertEquals(1, result, "result != 1");
+            } catch (SQLException e) {
+                throw new RuntimeException("SELECT 1 FROM DUAL 쿼리 실행 중 오류 발생", e);
+            }
+        }
+    }
+}
