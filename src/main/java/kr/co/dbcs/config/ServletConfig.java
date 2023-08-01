b93ea862671ebccf3053724a65ef222d12c7617a
Kyle <prophet@hanyang.ac.kr>
2023-08-02 00:44:05 +0900

    feat(arch): arch conversion from xml to java annotation - delete: web.xml, dispatcher-servlet.xml, applicationContext.xml - add: WebConfig.java, ServletConfig.java

---
+++ src/main/java/kr/co/dbcs/config/ServletConfig.java
@@ -0,0 +1,36 @@
+package kr.co.dbcs.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"kr.co.dbcs.controller", "kr.co.dbcs.exception"})
+public class ServletConfig implements WebMvcConfigurer {
+    // dispatcher-servlet.xml을 대신하는 java class
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 정적 요소 매핑
+
+        registry.addResourceHandler("/static/**").addResourceLocations("/WEB-INF/static/");
+        registry.addResourceHandler("/images/**").addResourceLocations("/WEB-INF/static/images/");
+        registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/static/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/static/js/");
+    }
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+    }
+}
---
+++ src/main/java/kr/co/dbcs/config/WebConfig.java
@@ -0,0 +1,40 @@
+package kr.co.dbcs.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebConfig implements WebApplicationInitializer {
+    // web.xml을 대신하는 java class
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+
+        // Root application context 설정
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.register(RootConfig.class);
+        servletContext.addListener(new ContextLoaderListener(rootContext));
+
+        // DispatcherServlet 설정
+        AnnotationConfigWebApplicationContext dispatcherContext = new AnnotationConfigWebApplicationContext();
+        dispatcherContext.register(ServletConfig.class);
+
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(dispatcherContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+        dispatcher.setInitParameter("throwExceptionIfNoHandlerFound", "true");
+
+        // EncodingFilter 설정
+        FilterRegistration.Dynamic encodingFilter = servletContext.addFilter("encodingFilter", new CharacterEncodingFilter());
+        encodingFilter.setInitParameter("encoding", "UTF-8");
+        encodingFilter.setInitParameter("forceEncoding", "true");
+        encodingFilter.addMappingForUrlPatterns(null, false, "/*");
+    }
+}
