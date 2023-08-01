e0374e557c592d222cc677b9fe2e810c09e82d84
Kyle <prophet@hanyang.ac.kr>
2023-08-01 09:01:46 +0900

    feat(aop): Add LogAdvice

---
+++ src/main/java/kr/or/sw/aop/LogAdvice.java
@@ -0,0 +1,13 @@
+package kr.or.sw.aop;
+
+import lombok.extern.log4j.Log4j2;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+@Aspect
+public class LogAdvice {
+
+
+}
