ca1eb71a54cf6c7e138c12c74d44065e1ae428bb
Kyle <prophet@hanyang.ac.kr>
2023-09-08 17:55:21 +0900

    fix(crawling): add movie info

--- src/main/java/kr/co/dbcs/model/MovieVO.java
+++ src/main/java/kr/co/dbcs/model/MovieVO.java
@@ -9,14 +9,20 @@ import org.springframework.stereotype.Component;
 public class MovieVO {
 
     @JsonProperty("movieNm")
-    private String movieName;
+    private String movieNm; // 영화제목
+
+    @JsonProperty("brchNm")
+    private String brchNm;  // 지점
+
+    @JsonProperty("theabExpoNm")
+    private String theabExpoNm; // 상영관
 
     @JsonProperty("playStartTime")
-    private String playStartTime;
+    private String playStartTime;   // 시작시간
 
     @JsonProperty("restSeatCnt")
-    private int restSeatCnt;
+    private int restSeatCnt;    // 잔여좌석
 
     @JsonProperty("totSeatCnt")
-    private int totSeatCnt;
+    private int totSeatCnt; // 전체좌석
 }
