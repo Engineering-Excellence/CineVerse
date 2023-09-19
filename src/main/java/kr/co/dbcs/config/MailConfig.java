a5994882af3aaa63898dd0e2e0bd51cea7db25e2
Kyle <prophet@hanyang.ac.kr>
2023-09-19 19:45:38 +0900

    ci(git-secret): encode mail properties

--- src/main/java/kr/co/dbcs/config/MailConfig.java
+++ src/main/java/kr/co/dbcs/config/MailConfig.java
@@ -1,38 +1,49 @@
 package kr.co.dbcs.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.lang.NonNull;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import java.util.Objects;
 import java.util.Properties;
 
 @Configuration
+@PropertySource("classpath:mail.properties")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class MailConfig {
 
-	@Bean
-	public JavaMailSender NaverMailService() {
+    private final Environment env;
 
-		JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-		javaMailSender.setHost("smtp.naver.com");
-		javaMailSender.setUsername("kjy970715");  // 네이버 아이디
-		javaMailSender.setPassword("88XZ9LQCJP2V");  // 네이버 비번
-		javaMailSender.setPort(465);
-		javaMailSender.setJavaMailProperties(getMailProperties());
-		
-		return javaMailSender;
-	}
+    @Bean
+    public JavaMailSender naverMailService() {
 
-	private Properties getMailProperties() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost(env.getProperty("mail.host"));
+        javaMailSender.setUsername(env.getProperty("mail.username"));  // 네이버 아이디
+        javaMailSender.setPassword(env.getProperty("mail.password"));  // 네이버 비번
+        javaMailSender.setPort(Integer.parseInt(Objects.requireNonNull(env.getProperty("mail.port"))));
+        javaMailSender.setJavaMailProperties(getMailProperties());
 
-		Properties properties = new Properties();
-		properties.setProperty("mail.transport.protocol","smtp");
-		properties.setProperty("mail.smtp.auth", "true");
-		properties.setProperty("mail.smtp.starttls.enable", "true");
-		properties.setProperty("mail.debug", "true");
-		properties.setProperty("mail.smtp.ssl.trust", "smtp.naver.com");
-		properties.setProperty("mail.smtp.ssl.enable", "true");
+        return javaMailSender;
+    }
 
-		return properties;
-	}
+    @NonNull
+    private Properties getMailProperties() {
+
+        Properties properties = new Properties();
+        properties.setProperty("mail.transport.protocol", env.getProperty("mail.transport.protocol"));
+        properties.setProperty("mail.smtp.auth", env.getProperty("mail.smtp.auth"));
+        properties.setProperty("mail.smtp.starttls.enable", env.getProperty("mail.smtp.starttls.enable"));
+        properties.setProperty("mail.debug", env.getProperty("mail.debug"));
+        properties.setProperty("mail.smtp.ssl.trust", env.getProperty("mail.smtp.ssl.trust"));
+        properties.setProperty("mail.smtp.ssl.enable", env.getProperty("mail.smtp.ssl.enable"));
+
+        return properties;
+    }
 }
