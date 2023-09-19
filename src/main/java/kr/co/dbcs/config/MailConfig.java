a5dbd5ed7cb05db6cdf8f03d83c36f12bea487d7
Kyle <prophet@hanyang.ac.kr>
2023-09-19 18:59:31 +0900

    fix(board): board paging hot-fix

--- src/main/java/kr/co/dbcs/config/MailConfig.java
+++ src/main/java/kr/co/dbcs/config/MailConfig.java
@@ -1,31 +1,30 @@
 package kr.co.dbcs.config;
 
-import java.util.Properties;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 @Configuration
 public class MailConfig {
 
 	@Bean
 	public JavaMailSender NaverMailService() {
-		JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 
+		JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 		javaMailSender.setHost("smtp.naver.com");
 		javaMailSender.setUsername("kjy970715");  // 네이버 아이디
 		javaMailSender.setPassword("88XZ9LQCJP2V");  // 네이버 비번
-		
 		javaMailSender.setPort(465);
-		
 		javaMailSender.setJavaMailProperties(getMailProperties());
 		
 		return javaMailSender;
 	}
 
 	private Properties getMailProperties() {
+
 		Properties properties = new Properties();
 		properties.setProperty("mail.transport.protocol","smtp");
 		properties.setProperty("mail.smtp.auth", "true");
